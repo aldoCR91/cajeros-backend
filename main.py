10ca66ac19d3486f201bd9ab9aefbb42b8740204
Aldo Burgos <altersystem.pro@gmail.com>
2023-06-18 23:49:42 -0600

    Flask Server configurado

--- main.py
+++ main.py
@@ -41,14 +41,16 @@
 
         3 - C R U D
 """
+from flask import Flask
+from flask_socketio import SocketIO
 import sqlite3
 
-con = sqlite3.connect("cajeros.db")
-cur = con.cursor()
+# con = sqlite3.connect("cajeros.db")
+# cur = con.cursor()
 
 # Crear tablas en la base de datos cajeros.db
 #cur.execute("CREATE TABLE usuarios(name, email, pin)")
-cur.execute("CREATE TABLE cajeros( codigo, stado, disponible)")
+#cur.execute("CREATE TABLE cajeros( codigo, stado, disponible)")
 
 # cur.execute("""
 #     INSERT INTO usuarios VALUES
@@ -56,22 +58,37 @@ cur.execute("CREATE TABLE cajeros( codigo, stado, disponible)")
 #         ('Aldo', 'aldo@cuc.cr', 1234)
 # """)
 
-cur.execute("""
-    INSERT INTO cajeros VALUES
-        ('01', 'ocupado', 10000000),
-        ('02', 'disponible', 7500000)
-""")
+# cur.execute("""
+#     INSERT INTO cajeros VALUES
+#         ('01', 'ocupado', 10000000),
+#         ('02', 'disponible', 7500000)
+# """)
             
-con.commit()
+# con.commit()
             
 #res = cur.execute("SELECT * FROM cajeros")
 # res.fetchall()
 
-for row in cur.execute("SELECT * FROM cajeros"):
-    print(row)
+# for row in cur.execute("SELECT * FROM cajeros"):
+#     print(row)
 
-#print(res)
-con.close()
+# #print(res)
+# con.close()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return "Hola desde flask"
+
+@socketio.on('message')
+def handle_message(msg):
+    print("Message: " + msg)
+
+if __name__ == '__main__':
+    socketio.run(app)
 
 
 #con.commit()
